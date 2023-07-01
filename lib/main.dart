84d7889a473d7eae3e72ff647e8eb259e33c8f9c
xuliang <xuliangjj@sina.cn>
2023-07-01 15:27:28 +0800

    init flutter

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Me',
+      home: Scaffold(
+        body: Center(
+          child: Text('Demo Page'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
